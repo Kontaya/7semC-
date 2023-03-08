de43a64bd86ba4466a0a2fbe62df64573596de9a
Kondratyeva Tatyana <t-mo_39@outlook.com>
2023-03-08 12:28:46 +0300

    dz

---
+++ dz47/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+int convertNumberToInt (string message)
+{
+  Console.WriteLine (message);
+  string str = Console.ReadLine() ?? String.Empty;
+  double numberDouble = Math.Abs(Convert.ToDouble(str));
+  int result = (int) numberDouble;
+  System.Console.WriteLine($"Your number is reduced to: {result}");
+  return result;
+}
+double [,] FillMatrix (int rowIn, int collIn)
+{
+  double [,] resultMatrix = new double [rowIn, collIn];
+  Random rnd = new Random();
+  for (int i = 0; i < rowIn; i++)
+  {
+    for (int j = 0; j < collIn; j++)
+    {
+      resultMatrix [i,j] = (double) rnd.Next(-100,101)/10;
+    }
+  }
+  return resultMatrix;
+}
+void ShowRealMatrix (double [,] matrixIn)
+{
+  System.Console.WriteLine("Your matrix filled withrandom real numbers:");
+  for (int i = 0; i < matrixIn.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrixIn.GetLength(1); j++)
+    {
+      System.Console.Write($"{matrixIn[i,j]}\t");
+    }
+    System.Console.WriteLine();
+  }
+}
+void MatrixFillRandomRealNumbers ()
+{
+  int numberRow = convertNumberToInt("Enter the number of rows of the real matrix");
+  int numberColl = convertNumberToInt("Enter the of columns array of the real matrix");
+  double [,] useMatrix = FillMatrix(collIn:numberColl, rowIn:numberRow);
+  ShowRealMatrix (useMatrix);
+}
+MatrixFillRandomRealNumbers ();
